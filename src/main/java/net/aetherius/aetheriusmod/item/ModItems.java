388af250b9fb75a0e21e0b85e296161ab210a1c9
juliawcaroline <juliacavalcantixz@gmail.com>
2025-05-10 19:09:04 -0300

    moedas(rubi e cyannaz) + as 8 chaves (falta tradução em ingels)

--- src/main/java/net/aetherius/aetheriusmod/item/ModItems.java
+++ src/main/java/net/aetherius/aetheriusmod/item/ModItems.java
@@ -25,8 +25,30 @@ public class ModItems {
             () -> new Item(new Item.Properties()));
     public static final DeferredItem<Item> AEDA_AMETISTA = ITEMS.register("aeda_ametista",
             () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> AEDA_RUBI = ITEMS.register("aeda_rubi",
+            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> AEDA_CYANNAZ = ITEMS.register("aeda_cyannaz",
+            () -> new Item(new Item.Properties()));
 
 
+
+
+    public static final DeferredItem<Item> CHAVERACHA = ITEMS.register("cyan_black_key",
+            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> CHAVERACHA2 = ITEMS.register("cyan_golden_key",
+            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> CHAVERACHA3 = ITEMS.register("red_black_key",
+            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> CHAVERACHA4 = ITEMS.register("red_golden_key",
+            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> CHAVERACHA5 = ITEMS.register("green_black_key",
+            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> CHAVERACHA6 = ITEMS.register("green_golden_key",
+            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> CHAVERACHA7 = ITEMS.register("purple_black_key",
+            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> CHAVERACHA8 = ITEMS.register("purple_golden_key",
+            () -> new Item(new Item.Properties()));
 
 
 
